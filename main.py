ec546ca9d50b4458ffdafc9228e90c4cdf37b293
Nazar <117998895+mandrake00@users.noreply.github.com>
2022-11-25 17:41:46 +0200

    Update main.py
    Создание мяча, создание функции передвижения мяча и отбивание мяча от стенок, добавлено скеилинг изображения

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ import os
 pygame.init()
 pygame.font.init()
 
-font = pygame.font.Font(None, 70)
-PATH= os.path.dirname(__file__) + os.sep
-window_w = 1280
-window_h = 720
-window = pygame.display.set_mode((window_w, window_h))
-tik = pygame.time.Clock()
-game = True
-
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, w,h, x,y, speed):
         super().__init__()
-        self.image = image
+        self.image = pygame.transform.scale(pygame.image.load(image), (w,h))
         self.w = w
         self.h = h 
         self.x = x
@@ -23,7 +15,7 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x,y, w,h)
 
     def show(self):
-        window.blit(pygame.image.load(self.image), (self.x, self.y))
+        window.blit(self.image, (self.x, self.y))
 
 
 class Racket(GameSprite):
@@ -43,9 +35,41 @@ class Racket(GameSprite):
 class Ball(GameSprite):
     def __init__(self, image, w,h, x,y, speed):
         super().__init__(image, w,h, x,y, speed)
+        self.directX = 1
+        self.directY = 1
 
-platform1 = Racket(PATH+'platform_placeholder.jpeg', 10, 100, 10, 10, 10, pygame.K_w, pygame.K_s)
-platform2 = Racket(PATH+'platform_placeholder.jpeg', 10, 100, window_w-40, window_h-210, 10, pygame.K_UP, pygame.K_DOWN)
+
+    def move(self):
+        self.x += self.speed*self.directX
+        self.y += self.speed*self.directY
+
+        if self.rect.y >= window_h - 30:
+            self.directY = -1
+        elif self.rect.y <= 0:
+            self.directY = 1
+
+        if self.rect.x >= window_w - 30:
+            self.directX = -1
+        elif self.rect.x<= 0:
+            self.directX = 1
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+
+
+font = pygame.font.Font(None, 70)
+PATH= os.path.dirname(__file__) + os.sep
+window_w = 1280
+window_h = 720
+window = pygame.display.set_mode((window_w, window_h))
+tik = pygame.time.Clock()
+game = True
+
+platform1 = Racket(PATH+'platform_placeholder.jpeg', 30, 200, 10, 10, 10, pygame.K_w, pygame.K_s)
+platform2 = Racket(PATH+'platform_placeholder.jpeg', 30, 200, window_w-40, window_h-210, 10, pygame.K_UP, pygame.K_DOWN)
+ball = Ball(PATH+"ball_placeholder.png", 50,50, 635, 300, 15)
 
 while game:
     window.blit(pygame.transform.scale(pygame.image.load(PATH+'a.png'), (window_w, window_h)), (0,0))
@@ -56,6 +80,8 @@ while game:
     platform2.move()
     platform2.show()
 
+    ball.show()
+    ball.move()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
