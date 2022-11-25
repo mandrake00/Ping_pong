c563a74f63d1ad936048d6d14e6d1275a81be636
Nazar <117998895+mandrake00@users.noreply.github.com>
2022-11-25 17:05:26 +0200

    Update main.py
    Изменение управления, создание 2х платформ, добавление фона, отображение платформ и обработка передвижения

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ pygame.init()
 pygame.font.init()
 
 font = pygame.font.Font(None, 70)
-
 PATH= os.path.dirname(__file__) + os.sep
-
 window_w = 1280
 window_h = 720
-
 window = pygame.display.set_mode((window_w, window_h))
-
 tik = pygame.time.Clock()
 game = True
 
@@ -27,26 +23,40 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x,y, w,h)
 
     def show(self):
-        window.blit(pygame.transform.scale(pygame.image.load(self.image), (self.w, self.h)), (self.x, self.y))
+        window.blit(pygame.image.load(self.image), (self.x, self.y))
 
 
 class Racket(GameSprite):
-    def __init__(self, image, w,h, x,y, speed):
+    def __init__(self, image, w,h, x,y, speed, keyup, keydown):
         super().__init__(image, w,h, x,y, speed)
+        self.keyup = keyup
+        self.keydown = keydown
         
-    def ws(self):
+    def move(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_DOWN] == True:
-            self.rect.y += self.speed
-        if keys[pygame.K_UP] == True:
-            self.rect.y -= self.speed
-
+        if keys[self.keydown] == True:
+            self.y += self.speed
+        if keys[self.keyup] == True:
+            self.y -= self.speed
+        self.rect.y = self.y
+        
 class Ball(GameSprite):
     def __init__(self, image, w,h, x,y, speed):
         super().__init__(image, w,h, x,y, speed)
 
+platform1 = Racket(PATH+'platform_placeholder.jpeg', 10, 100, 10, 10, 10, pygame.K_w, pygame.K_s)
+platform2 = Racket(PATH+'platform_placeholder.jpeg', 10, 100, window_w-40, window_h-210, 10, pygame.K_UP, pygame.K_DOWN)
 
 while game:
+    window.blit(pygame.transform.scale(pygame.image.load(PATH+'a.png'), (window_w, window_h)), (0,0))
+
+    platform1.move()
+    platform1.show()
+
+    platform2.move()
+    platform2.show()
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
